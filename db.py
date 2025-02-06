e27745feb0cccc6a2f8cf40cfd4fddd113c5595e
SEKHON0704 <113328562+SEKHON0704@users.noreply.github.com>
2025-02-06 23:02:53 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,12 @@
+# In-memory data storage
+products_data = []
+customers_data = []
+sales_data = []
+
+def load_data():
+    # For simplicity, we are using in-memory data. You can use a file/database to store data.
+    pass
+
+def save_data():
+    # In this case, we just print to the console. But data could be saved to a file/database here.
+    print("Data saved successfully.")
